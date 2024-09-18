438c6271e3c84e3f55268d35450d56a50db8ed59
ltdwsl <lethanhdat.real@gmail.com>
2024-09-18 16:33:16 +0700

    init project

--- nvim/init.lua
+++
@@ -1,4 +0,0 @@
--- init.lua
-require("config.base")
-require("config.map")
-require("config.lazy")
--- nvim/lua/plugins/split-term.lua
+++
@@ -1,9 +0,0 @@
-return {
-	'akinsho/toggleterm.nvim',
-	version = "*",
-	config = function()
-		require('toggleterm').setup()
-		vim.keymap.set('n', '<leader>v', ':ToggleTerm<CR>', { noremap = true, silent = true })
-		-- vim.keymap.set('n', '<C-`>', ':ToggleTerm<CR>', { noremap = true, silent = true })
-	end
-}
